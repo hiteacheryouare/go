d0e19b52a8c21c04e6678a159c681297f900ca04
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-12-01 09:19:45 -0500

    slowly getting there

--- lib/main.go
+++ lib/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
+	"os"
+	"runtime"
 )
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/hello", sayHello)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, welcome to %s!", r.URL.Path[1:])
+	var root string = "~/souce/repos/go/"
+	fileSystem := os.DirFS(root)
+	fs.ReadFile(fileSystem, "./pgaes/index.html")
+	w.WriteHeader(200)
+	w.Header().Add("server", runtime.Version())
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	fmt.Fprintf(w, "Hello there, %s!, Welcome to the web.", name)
 }
